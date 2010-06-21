e83ad11a085da7c9f5e2e75af7f1d07a1a60f8eb
Robert Pitts <rbxbxdev@gmail.com>
2010-06-21 16:17:29 -0400

    refiggidyfactorize

--- lib/scissor/echonest.rb
+++ lib/scissor/echonest.rb
@@ -10,52 +10,62 @@ module Scissor
   class Chunk
     class << self
       attr_accessor :echonest_api_key
+    end
 
-      def echonest
-        @echonest ||= Echonest(echonest_api_key)
-      end
+    def self.echonest
+      @echonest ||= Echonest(echonest_api_key)
     end
 
     def beats
-      tempfile_for_echonest do |tmpfile|
-        chunks = []
-        scissor = to_file(tmpfile, :bitrate => '64k')
+      scissor_to_file
 
-        beats = self.class.echonest.get_beats(tmpfile)
-
-        if beats.size != 0
-          chunk = self[0, beats.first.start]
-          beat = Beat.new(beats.first.start, 1.0)
-          chunk.set_delegate(beat)
-          chunks << chunk
-        end
-
-        beats.inject do |m, beat|
-          chunk = self[m.start, beat.start - m.start]
-          chunk.set_delegate(m)
-          chunks << chunk
-          beat
-        end
-
-        chunks
+      if get_beats.any?
+        append_beats_to([first_chunk])
+      else
+        append_beats_to([])
       end
     end
 
     def segments
-      tempfile_for_echonest do |tmpfile|
-        scissor = to_file(tmpfile, :bitrate => '64k')
-
-        segments = self.class.echonest.get_segments(tmpfile)
-        segments.inject([]) do |chunks, segment|
-          chunk = self[segment.start, segment.duration]
-          chunk.set_delegate(segment)
-          chunks << chunk
-          chunks
-        end
+      scissor_to_file
+      get_segments.map do |segment|
+        chunk = self[segment.start, segment.duration]
+        chunk.set_delegate(segment)
+        chunk
       end
     end
 
     private
+
+    def beat
+      @beat ||= Beat.new(get_beats.first.start, 1.0)
+    end
+
+    def first_chunk
+      chunk = self[0, get_beats.first.start]
+      chunk.set_delegate(beat)
+    end
+
+    def append_beats_to(chunks)
+      get_beats.inject do |m, beat|
+        chunk = self[m.start, beat.start - m.start]
+        chunk.set_delegate(m)
+        chunks << chunk
+        beat
+      end
+    end
+
+    def scissor_to_file
+      to_file(tempfile_for_echonest, :bitrate => '64k')
+    end
+
+    def get_beats
+      @get_beats ||= Chunk.echonest.get_beats(tempfile_for_echonest)
+    end
+
+    def get_segments
+      @get_segments ||= Chunk.echonest.get_segments(tempfile_for_echonest)
+    end
 
     def tempfile_for_echonest
       tmpfile = Pathname.new('/tmp/scissor_echonest_temp_' + $$.to_s + '.mp3')
